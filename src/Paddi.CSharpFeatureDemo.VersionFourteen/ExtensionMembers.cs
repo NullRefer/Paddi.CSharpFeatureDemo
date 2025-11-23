4360b97ad6eabaee4c95d6d05853f29ef4f01686
paddi <a953195910@outlook.com>
2025-11-23 10:20:02 +0800

    update C#14 features

--- src/Paddi.CSharpFeatureDemo.VersionFourteen/ExtensionMembers.cs
+++ src/Paddi.CSharpFeatureDemo.VersionFourteen/ExtensionMembers.cs
@@ -6,24 +6,30 @@ public static class ExtensionMembers
     {
         int[] array = [1, 2, 3, 4, 5];
         Console.WriteLine(array.IsEmpty); // Output: False
-        Console.WriteLine(IEnumerable<int>.IsEmptyMethod(array)); // Output: False
+        Console.WriteLine(array.IsEmptyMethod()); // Output: False
+        Console.WriteLine(IEnumerable<int>.ExtendedIsEmptyMethod(array)); // Output: False
+        Console.WriteLine(array.NormalIsEmptyMethod()); // Output: False
     }
 }
 
 public static class EnumerableExtensions
 {
-    // Extension property for class
+    // Extension property and method for class
     extension<T>(IEnumerable<T> source)
     {
         public bool IsEmpty => !source.Any();
+        public bool IsEmptyMethod() => !source.Any();
     }
 
     // Extension static method for class
     extension<T>(IEnumerable<T>)
     {
-        public static bool IsEmptyMethod(IEnumerable<T> source)
+        public static bool ExtendedIsEmptyMethod(IEnumerable<T> source)
         {
             return !source.Any();
         }
     }
+    
+    // Normal static method for class
+    public static bool NormalIsEmptyMethod<T>(this IEnumerable<T> source) => !source.Any();
 }
